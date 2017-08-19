ab9deb546e70f8ac3580d87c782bad63e7951d53
jzchatbot <31149723+jzchatbot@users.noreply.github.com>
2017-08-19 14:22:41 -0500

    .

--- main.js
+++ main.js
@@ -34,8 +34,7 @@ router.get('/', function(req, res) {
   request.on('response', function(response) {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Pragma, If-Modified-Since")
-    response.setHeader("Pragma", "*")
-    response.setHeader("If-Modified-Since", "*")
+
     //  console.log(response);
     res.json(response);
   });
@@ -49,8 +48,7 @@ router.get('/', function(req, res) {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Pragma", "*")
-  res.header("If-Modified-Since", "*")
+
   next();
 });
 app.use('/api/text/*', router);
