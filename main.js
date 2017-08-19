98b2150dc7faced8ebd517f98035d35c681d5054
jzchatbot <31149723+jzchatbot@users.noreply.github.com>
2017-08-19 14:12:05 -0500

    test again

--- main.js
+++ main.js
@@ -34,6 +34,8 @@ router.get('/', function(req, res) {
   request.on('response', function(response) {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+    response.setHeader("Pragma", "*")
+    response.setHeader("If-Modified-Since", "*")
     //  console.log(response);
     res.json(response);
   });
@@ -48,7 +50,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Pragma", "*")
-  res.header("If-Modified-Since", "*") 
+  res.header("If-Modified-Since", "*")
   next();
 });
 app.use('/api/text/*', router);
